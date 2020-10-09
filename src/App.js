5331c5f52ede2296d96524784be4a7fbdc53c645
JFelixZuniga <johnfzuniga@gmail.com>
2020-10-09 19:05:45 -0300

    Comenzando Task App

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
 function App() {
+  const [userName, setUserName] = useState("JFZuniga");
+  const [taskItems, setTaskItems] = useState([
+    { name: "Task one", done: false },
+    { name: "Task two", done: false },
+    { name: "Task three", done: true },
+    { name: "Task four", done: false },
+  ]);
+
+  const taskTableRows = () => {
+    return taskItems.map((task) => (
+      <tr key={task.name}>
+        <td>{task.name}</td>
+      </tr>
+    ));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Hola mundo</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>Description</th>
+            <th>Done</th>
+          </tr>
+        </thead>
+        <tbody>{taskTableRows()}</tbody>
+      </table>
     </div>
   );
 }
